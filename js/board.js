6b9ac14144b2f2c2753e091eb763703b9382e7b6
Tran Bao Hoang <23130118@st.hcmuaf.edu.vn>
2026-09-24 20:31:53 +0700

    xử lý nút đi xuống nghĩa là hình vuông sẽ có 4 ô cho let =4 và ban đầu sẽ draw trước xong sau đó khi nhận sự kiện down thì sẽ xoá position cũ sau đó postion+=10 và draw lại

--- js/board.js
+++ js/board.js
@@ -1,4 +1,4 @@
-const board = document.querySelector("board");
+const board = document.querySelector(".board");
 const row = 20;
 const col = 10;
 for (let i = 0; i < row * col; i++) {
@@ -6,3 +6,33 @@ for (let i = 0; i < row * col; i++) {
   matrix.classList.add("matrix");
   board.appendChild(matrix);
 }
+// xu ly mau hinh vuong
+const matrixs = document.querySelectorAll(".matrix");
+matrixs[4].classList.add("block", "block-square");
+matrixs[5].classList.add("block", "block-square");
+matrixs[14].classList.add("block", "block-square");
+matrixs[15].classList.add("block", "block-square");
+// xu ly nut di xuong
+// nghĩa là ban đầu vị trí của hình vuông là 4 và khi nhấn nút xuốngvị trí sẽ tăng lên 10 (vì có 10 cột) do đó hình vuông sẽ di chuyển xuống một hàng. Khi nhấn nút xuống thì cần xóa các ô vuông hiện tại và thêm các ô vuông mới ở vị trí mới.
+let position = 4;
+function clearSquare() {
+  matrixs[position].classList.remove("block", "block-square");
+  matrixs[position + 1].classList.remove("block", "block-square");
+  matrixs[position + 10].classList.remove("block", "block-square");
+  matrixs[position + 11].classList.remove("block", "block-square");
+}
+
+function drawSquare() {
+  matrixs[position].classList.add("block", "block-square");
+  matrixs[position + 1].classList.add("block", "block-square");
+  matrixs[position + 10].classList.add("block", "block-square");
+  matrixs[position + 11].classList.add("block", "block-square");
+}
+drawSquare();
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowDown") {
+    clearSquare();
+    position += 10;
+    drawSquare();
+  }
+});
